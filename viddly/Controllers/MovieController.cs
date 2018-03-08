944252f6416bdfb10d144cbff5b94f4997b94734
João Nascimento <joao1991algoritmo@gmail.com>
2018-03-08 14:04:05 -0300

    add estilo boostrap lumen do site boostrapwach; view rambom;controller movie e viewModel movie

---
+++ viddly/Controllers/MovieController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using viddly.Models;
+
+namespace viddly.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Random()
+        {
+            var movieViewModel = new MovieViewModel() { Name="Shrek!!" };
+            return View(movieViewModel);
+        }
+    }
+}
